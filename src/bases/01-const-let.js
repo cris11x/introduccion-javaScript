8f34385694c6cc4213f944e5b40235063db6d890
Cristian <cris10x@hotmail.com>
2021-03-04 11:51:35 -0500

    Const y let

---
+++ src/bases/01-const-let.js
@@ -0,0 +1,22 @@
+// variables y ocnstantes
+
+/* # No utilizar var
+solo utilizar const y let */
+
+const nombre = 'Cristian';
+const apellido = 'Fajardo';
+
+let valorDado = 5;
+valorDado = 4;
+
+console.log( nombre, apellido, valorDado );
+
+if(true) {
+    const nombre = 'Piter';
+    let valorDado = 6;
+
+    console.log(nombre);
+}
+
+console.log(valorDado);
+
